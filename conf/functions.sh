be9dc232aa4f92719538f73a48e28c8a616f517a
Justin Riddiough <justinriddiough@stanford.edu>
2018-03-02 00:41:57 -0800

    Configuration cleanup

---
+++ conf/functions.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+import_database() {
+  echo "[Build] Import the database"
+  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf -e "DROP DATABASE $DB_NAME;"
+  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf -e "CREATE DATABASE $DB_NAME;"
+  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf $DB_NAME < $BUILD_DIR/prod.sql
+}
+
+copy_make_files_to_www() {
+  echo "[Build] Copying files from last build"
+  rm -Rf $BUILD_WWW_DIR
+  mkdir -p $BUILD_WWW_DIR
+  cp -fr $BUILD_MAKE_DIR $BUILD_WWW_DIR
+}
+
+post_database_operations() {
+  echo "[Build] Set site variables"
+  cd $BUILD_WWW_DIR/gsb_public
+  php $DRUSH_PATH upwd --password=admin admin
+  php $DRUSH_PATH vset file_temporary_path $BUILD_TMP_DIR
+  php $DRUSH_PATH vset cache 0
+  php $DRUSH_PATH vset preprocess_css 0
+  php $DRUSH_PATH vset preprocess_js 0
+  php $DRUSH_PATH vset error_level 2
+
+  echo "[Build] Disable Memcache, Acquia and Shield"
+  php $DRUSH_PATH dis -y memcache_admin acquia_purge acquia_agent shield
+
+  echo "[Build] Run database updates"
+  php $DRUSH_PATH updb -y
+
+  echo "[Build] Revert features"
+  #php $DRUSH_PATH fra -y
+
+  #echo "Enable Stage File Proxy and Devel"
+  php $DRUSH_PATH en -y devel stage_file_proxy
+  php $DRUSH_PATH vset stage_file_proxy_origin $REMOTE_URL
+
+  echo "[Build] Enable Kraken"
+  php -r "print json_encode(array('api_key'=> '$KRAKEN_KEY', 'api_secret'=> '$KRAKEN_SECRET'));" | $DRUSH_PATH vset --format=json kraken -
+
+  echo "[Build] Enable views development setup."
+  php $DRUSH_PATH vd
+}
+
+configure_drupal_sites_dir() {
+  echo "[Build] Configure drupal sites/default directory"
+  rm -Rf $BUILD_MAKE_DIR/sites/default
+  cp -R $SCRIPT_DIR/assets/$DISTRO/default $BUILD_MAKE_DIR/sites
+  ln -s $BUILD_MAKE_DIR/sites/default $BUILD_MAKE_DIR/sites/gsb
+  chmod -R 777 $BUILD_MAKE_DIR/sites
+  echo "[Build] .htaccess to prevent https:// in development"
+  cp $SCRIPT_DIR/assets/.htaccess $BUILD_MAKE_DIR/.htaccess
+
+  # replace database credentials
+  if [[ $platform == 'linux' ]]; then
+    SED_CMD="-i"
+  elif [[ $platform == 'macos' ]]; then
+    SED_CMD="-i .bk"
+  fi
+  sed $SED_CMD "s/---db-username---/$DB_USERNAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
+  sed $SED_CMD "s/---db-password---/$DB_PASS/g" $BUILD_MAKE_DIR/sites/default/settings.php
+  sed $SED_CMD "s/---db-name---/$DB_NAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
+  sed $SED_CMD "s/---db-url---/$DB_URL/g" $BUILD_MAKE_DIR/sites/default/settings.php
+  sed $SED_CMD "s/---db-port---/$DB_PORT/g" $BUILD_MAKE_DIR/sites/default/settings.php
+}
+
+get_gsb-public_distro() {
+  echo "[Build] Get the gsb_public distribution file"
+  # Setup the distro directory.
+  if [ ! -d "$BUILD_DISTRO_DIR" ]; then
+    cd $BUILD_DIR
+    git clone $DISTRO_GIT_URL
+  fi
+  # Checkout chosen branch.
+  cd $BUILD_DISTRO_DIR
+  git pull
+  git checkout $BRANCH
+}
+
+run_drush_make() {
+  echo "[Build] Running Drush Make."
+  rm -Rf $BUILD_MAKE_DIR
+  php $DRUSH_PATH make --working-copy $BUILD_DISTRO_DIR/$DISTRO-distro.make $BUILD_MAKE_DIR
+}
+
+export_db_from_acquia() {
+  echo "[Build] Export database from Acquia"
+  php $DRUSH_PATH @$SITE_ALIAS.prod sql-dump --structure-tables-list="cache,cache_*,history,search_*,sessions,watchdog" > $BUILD_DIR/prod.sql
+  wait
+}
+
+convertsecs() {
+  m=$(($1 % 3600 / 60))
+  s=$(($1 % 60))
+  printf "%02d:%02d\n" $m $s
+}
--- gsb-public-build.sh
+++ gsb-public-build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-convertsecs() {
-  m=$(($1 % 3600 / 60))
-  s=$(($1 % 60))
-  printf "%02d:%02d\n" $m $s
-}
-
 # Get global variables
 if [ -e conf/global.cfg ]; then
   source conf/global.cfg
@@ -15,7 +9,6 @@ fi
 
 if [ -e conf/lastbranch.cfg ]; then
   source conf/lastbranch.cfg
-  DEFAULT_BRANCH = $LAST_BRANCH
 fi
 
 # Location of where we put the build artifacts.
@@ -32,35 +25,37 @@ BUILD_MAKE_DIR=$BUILD_DIR/gsb_public
 BUILD_TMP_DIR=$BUILD_DIR/drupal-tmp
 BUILD_WWW_DIR=$SCRIPT_DIR/src
 
+source conf/functions.sh
+
 # Check for missing variables.
 ERROR=false
 if [ -z $DRUSH_PATH ]; then
-  echo "The \$DRUSH_PATH variable needs to be set."
+  echo "[Build] The \$DRUSH_PATH variable needs to be set."
   ERROR=true
 fi
 
 if [ -z $BUILD_WWW_DIR ]; then
-  echo "The \$BUILD_WWW_DIR variable needs to be set."
+  echo "[Build] The \$BUILD_WWW_DIR variable needs to be set."
   ERROR=true
 fi
 
 if [ -z $BUILD_TMP_DIR ]; then
-  echo "The \$BUILD_TMP_DIR variable needs to be set."
+  echo "[Build] The \$BUILD_TMP_DIR variable needs to be set."
   ERROR=true
 fi
 
 if [ -z $DB_USERNAME ]; then
-  echo "The \$DB_USERNAME variable needs to be set."
+  echo "[Build] The \$DB_USERNAME variable needs to be set."
   ERROR=true
 fi
 
 if [ -z $DB_PASS ]; then
-  echo "The \$DB_PASS variable needs to be set."
+  echo "[Build] The \$DB_PASS variable needs to be set."
   ERROR=true
 fi
 
 if [ ! -w "$PROD_TMP" ]; then
-  echo "The temporary directory $PROD_TMP needs to exist and be writable."
+  echo "[Build] The temporary directory $PROD_TMP needs to exist and be writable."
   ERROR=true
 fi
 
@@ -69,21 +64,28 @@ if [ $ERROR = true ]; then
   exit
 fi
 
-echo "Build Options";
-echo "[1] Full Build: Runs make and downloads fresh database from production"
-echo "[2] Quick Build: Restores files and database from last full build"
-echo "[3] Restore files only: Restores files from last full build"
-echo "[4] Restore database only:  Restores database from last full build"
-echo "[5] Cancel"
+echo "-[Stanford GSB Public Website]----------------------------------------------"
+if [ -d "src/gsb_public/profiles/gsb_public" ]; then
+  cd src/gsb_public/profiles/gsb_public
+  git remote update
+  git status -uno
+fi
+echo "---[Build Options]----------------------------------------------------------";
+echo "   [1] Full Build: Runs make and downloads fresh database from production"
+echo "   [2] Quick Build: Restores files and database from last full build"
+echo "   [3] Files: Restores files from last full build"
+echo "   [4] Database: Restores database from last full build"
+echo "   [5] Database: Copy database from production, keep files as are"
+echo "   [6] Cancel"
 read -e -p ":" BUILD_OPTION
 
 if [ $BUILD_OPTION = 1 ]; then
-  read -e -p "Enter the branch to use [$DEFAULT_BRANCH]: " BRANCH
+  read -e -p "Enter the branch to use [$LAST_BRANCH]: " BRANCH
   # If nothing is specified then use the specified default.
   if [ ! -n "$BRANCH" ]; then
-    BRANCH=$DEFAULT_BRANCH
+    BRANCH=$LAST_BRANCH
   else
-    echo "LAST_BRANCH = $BRANCH" > conf/lastbranch.cfg
+    echo "LAST_BRANCH=$BRANCH" > conf/lastbranch.cfg
   fi
 fi
 
@@ -100,103 +102,41 @@ if [ ! -d "$BUILD_TMP_DIR" ]; then
 fi
 
 # Remove the old installation directory
-rm -Rf $BUILD_WWW_DIR
-
+# Start our counter.
+START_TIME=$SECONDS
 if [ $BUILD_OPTION = 1 ]; then
-  # Start our counter.
-  START_TIME=$SECONDS
-
-  # Setup the distro directory.
-  if [ ! -d "$BUILD_DISTRO_DIR" ]; then
-    cd $BUILD_DIR
-    git clone $DISTRO_GIT_URL
-  fi
-
-  # Checkout chosen branch.
-  cd $BUILD_DISTRO_DIR
-  git pull
-  git checkout $BRANCH
-
-  # Move into our apache root and run drush make and/or replace the db.
-  echo "Deleting Old $BUILD_MAKE_DIR"
-  rm -Rf $BUILD_MAKE_DIR
-  echo "Run drush make and dump the database. This can take upwards of 15 minutes."
-  php $DRUSH_PATH make --working-copy $BUILD_DISTRO_DIR/$DISTRO-distro.make $BUILD_MAKE_DIR
-
-  php $DRUSH_PATH @$SITE_ALIAS.$BASE_ENV sql-dump --structure-tables-list="cache,cache_*,history,search_*,sessions,watchdog" > $BUILD_DIR/$BASE_ENV.sql
-  wait
-  echo "Import the database"
-  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf -e "DROP DATABASE $DB_NAME;"
-  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf -e "CREATE DATABASE $DB_NAME;"
-  mysql --defaults-extra-file=$SCRIPT_DIR/conf/global-db.conf $DB_NAME < $BUILD_DIR/$BASE_ENV.sql
-  ELAPSED_TIME=$(($SECONDS - $START_TIME))
-  echo "Make Time: " $(convertsecs $ELAPSED_TIME)
-
-  rm -Rf $BUILD_MAKE_DIR/sites/default
-  cp -R $SCRIPT_DIR/assets/$DISTRO/default $BUILD_MAKE_DIR/sites
-  ln -s $BUILD_MAKE_DIR/sites/default $BUILD_MAKE_DIR/sites/gsb
-  chmod -R 777 $BUILD_MAKE_DIR/sites
-
-  # replace database credentials
-  if [[ $platform == 'linux' ]]; then
-    sed -i "s/---db-username---/$DB_USERNAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i "s/---db-password---/$DB_PASS/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i "s/---db-name---/$DB_NAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i "s/---db-url---/$DB_URL/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i "s/---db-port---/$DB_PORT/g" $BUILD_MAKE_DIR/sites/default/settings.php
-  elif [[ $platform == 'macos' ]]; then
-    sed -i .bk "s/---db-username---/$DB_USERNAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i .bk "s/---db-password---/$DB_PASS/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i .bk "s/---db-name---/$DB_NAME/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i .bk "s/---db-url---/$DB_URL/g" $BUILD_MAKE_DIR/sites/default/settings.php
-    sed -i .bk "s/---db-port---/$DB_PORT/g" $BUILD_MAKE_DIR/sites/default/settings.php
-  fi
-
-  cp $SCRIPT_DIR/assets/.htaccess $BUILD_MAKE_DIR/.htaccess
-
-  echo "BUILD Copy make files to apache folder"
-  if [ ! -d "$BUILD_WWW_DIR" ]; then
-    echo "Creating $BUILD_WWW_DIR"
-    mkdir -p $BUILD_WWW_DIR
-  fi
-  cp -fr $BUILD_MAKE_DIR $BUILD_WWW_DIR
+  echo "---[Full Build]-----"
+  get_gsb-public_distro
+  run_drush_make
+  configure_drupal_sites_dir
+  export_db_from_acquia
+  copy_make_files_to_www
+  import_database
+  post_database_operations
+elif [ $BUILD_OPTION = 2 ]; then
+  echo "---[Quick Build]-----"
+  copy_make_files_to_www
+  import_database
+  post_database_operations
+elif [ $BUILD_OPTION = 3 ]; then
+  echo "--[Restore files from last build]-----"
+  copy_make_files_to_www
+elif [ $BUILD_OPTION = 4 ]; then
+  echo "---[Restore database from last full build]-----"
+  import_database
+  post_database_operations
+elif [ $BUILD_OPTION = 5 ]; then
+  echo "---[Restore database from prod]-----"
+  export_db_from_acquia
+  import_database
+  post_database_operations
+elif [ $BUILD_OPTION = 6 ]; then
+  echo "---[Goodbye]-----"
+  exit
 fi
 
-echo "Set site variables"
 cd $BUILD_WWW_DIR/gsb_public
-php $DRUSH_PATH upwd --password=admin admin
-php $DRUSH_PATH vset file_temporary_path $BUILD_TMP_DIR
-php $DRUSH_PATH vset cache 0
-php $DRUSH_PATH vset preprocess_css 0
-php $DRUSH_PATH vset preprocess_js 0
-php $DRUSH_PATH vset error_level 2
-
-echo "Disable Memcache, Acquia and Shield"
-php $DRUSH_PATH dis -y memcache_admin acquia_purge acquia_agent shield
-
-echo "Run database updates"
-php $DRUSH_PATH updb -y
-
-echo "Revert features"
-php $DRUSH_PATH fra -y
-
-#echo "Enable Stage File Proxy and Devel"
-php $DRUSH_PATH en -y devel stage_file_proxy
-php $DRUSH_PATH vset stage_file_proxy_origin $REMOTE_URL
-
-echo "Enable Kraken"
-php -r "print json_encode(array('api_key'=> '$KRAKEN_KEY', 'api_secret'=> '$KRAKEN_SECRET'));" | $DRUSH_PATH vset --format=json kraken -
-
-echo "Enable views development setup."
-php $DRUSH_PATH vd
 php $DRUSH_PATH cc all
-
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
-echo "Total Time: " $(convertsecs $ELAPSED_TIME)
-
-# Send a notification saying we are done.
-echo "The build has completed successfully."
-echo "release built is: " $BRANCH > gsb_build_options.txt
-echo "base env is: " $BASE_ENV >> gsb_build_options.txt
-echo "db refresh is: " $REFRESH >> gsb_build_options.txt
-echo "use make is: " $USE_MAKE >> gsb_build_options.txt
+echo "[Build] Total Time: " $(convertsecs $ELAPSED_TIME)
+echo "[Build]  The build has completed successfully."
